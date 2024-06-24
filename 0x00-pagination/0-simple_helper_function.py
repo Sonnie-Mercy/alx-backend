557345f1aa7c973dc3304d6d68eeb05dea9c6d81
Mercy Muthoni <ndirangumercy4@gmail.com>
2024-06-24 18:17:49 +0000

    task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" 0-simple_helper_function.py """
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    function named index_range that takes two integer arguments
+    page and page_size
+    """
+    start = (page -1) * page_size
+    end = page * page_size
+    return (start, end)
